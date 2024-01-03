0637f5768a74d88aa2c43c376488792e5f937d01
Brian Merchant <bzm3r@proton.me>
2024-01-03 09:45:40 -0800

    structure of generalized matching system, which can handle both ItemUse and syn::Path

--- bpaf_derive/src/custom_path.rs
+++ bpaf_derive/src/custom_path.rs
@@ -1,6 +1,12 @@
+use std::{
+    clone,
+    iter::{Cloned, FromIterator, Map, Peekable},
+    slice,
+};
+
 use syn::{
     punctuated::{self, Punctuated},
-    token::PathSep,
+    token::{Match, PathSep},
     visit_mut::{self, VisitMut},
     Ident, Item, ItemUse, PathArguments, PathSegment, UseName, UsePath, UseRename, UseTree,
 };
@@ -9,356 +15,213 @@ use syn::{
 /// those crate [`Path`](syn::Path)s which match
 /// [`target`](Self::target) and replace them with [`replacement`](Self::replacement).
 pub(crate) struct BpafPathReplacer {
-    target: syn::Path,
+    query: syn::Path,
     replacement: syn::Path,
 }
 
-/// There are two kinds of "paths" we must deal with:[`syn::Path`] and
-/// [`syn::UseTree`] (which comes up when parsing a [`syn::ItemUse`]).
-///
-/// [`syn::Path`] has simple parts which are all [`Ident`]s, but a
-/// [`syn::UseTree`] might have other kinds of parts (see its documentation for details).
-pub enum PathPart<'a> {
-    /// Part of a path-like which is essentially an [`Ident`].
-    Ident(Box<&'a dyn IdentRef>),
-    Other(&'a UseTree),
-}
+impl BpafPathReplacer {
+    pub(crate) fn new(query: syn::Path, replacement: syn::Path) -> Self {
+        BpafPathReplacer { query, replacement }
+    }
 
-impl<'a> PartialEq for PathPart<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Ident(l), Self::Ident(r)) => l == r,
-            (Self::Other(l), Self::Other(r)) => l == r,
-            _ => false,
-        }
+    /// First checks if both [`query`](Self::query) and `other` have the
+    /// leading path segment (`::`, which marks [a path as
+    /// global](https://doc.rust-lang.org/reference/procedural-macros.html#procedural-macro-hygiene))
+    /// and the same [`Ident`]s forming the prefix of the path. If there is a
+    /// match, the prefix of `target` will be replacement with [`replacement`](Self::replacement)
+    fn replace_if_match<'a, P: CratePath>(&self, other: &'a P) -> Option<P> {
+        todo!()
     }
 }
 
-/// Marker trait representing references (mutable or immutable) to [`Ident`]s.
-pub(crate) trait IdentRef {
+pub struct Query {
+    leading_colon: Option<PathSep>,
+    segments: Vec<Ident>,
+}
+
+pub struct Replacement {
+    leading_colon: Option<PathSep>,
+    segments: Vec<Ident>,
+}
+
+impl Replacement {
+    fn path_parts_iter<X: PathPart>(&self) -> Map<Cloned<slice::Iter<'_, Ident>>, fn(Ident) -> X> {
+        self.segments.iter().cloned().map(<X as From<Ident>>::from)
+    }
+}
+
+pub struct Target<S: CratePath> {
+    leading_colon: Option<PathSep>,
+    segments: S,
+}
+
+pub trait PathPart: From<Ident> + Clone {
     fn ident(&self) -> &Ident;
 }
 
-impl IdentRef for &Ident {
-    fn ident(&self) -> &Ident {
-        &self
-    }
-}
-
-impl IdentRef for &mut Ident {
-    fn ident(&self) -> &Ident {
-        &self
-    }
-}
-
-impl IdentRef for &UsePath {
-    fn ident(&self) -> &Ident {
-        &self.ident
-    }
-}
-
-impl IdentRef for &UseName {
-    fn ident(&self) -> &Ident {
-        &self.ident
-    }
-}
-
-impl IdentRef for &UseRename {
-    fn ident(&self) -> &Ident {
-        &self.ident
-    }
-}
-
-/// Marker trait representing references (mutable or immutable) to [`UseTree`]s.
-pub(crate) trait TreeRef {}
-
-impl TreeRef for &UseTree {}
-
-impl TreeRef for &mut UseTree {}
-
-impl<'a> From<Id> for PathPart<'a> {
-    fn from(value: Box<&'a dyn IdentRef>) -> Self {
-        PathPart::Ident(value)
-    }
-}
-
-impl<'a, Id: IdentRef> From<&'a UseTree> for PathPart<'a, Id> {
-    fn from(value: &'a UseTree) -> Self {
-        PathPart::Other(value)
-    }
-}
-
-impl<'a, Id: IdentRef> From<&'a mut UseTree> for PathPart<'a, Id> {
-    fn from(value: &'a mut UseTree) -> Self {
-        PathPart::Other(value)
-    }
-}
-
-pub struct PathIter<'a> {
-    segments: punctuated::Iter<'a, PathSegment>,
-}
-
-impl<'a> Iterator for PathIter<'a> {
-    type Item = PathPart<'a, &'a Ident>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next) = self.segments.next() {
-            if PathArguments::None == next.arguments {
-                return Some((&next.ident).into());
-            }
-        }
-
-        None
-    }
-}
-
-pub struct PathIterMut<'a> {
-    segments: punctuated::IterMut<'a, PathSegment>,
-}
-
-impl<'a> Iterator for PathIterMut<'a> {
-    type Item = PathPart<'a, &'a mut Ident>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next) = self.segments.next() {
-            if PathArguments::None == next.arguments {
-                return Some((&mut next.ident).into());
-            }
-        }
-
-        None
-    }
-}
-
-pub struct UsePathIter<'a> {
-    tree: Option<&'a UseTree>,
-}
-
-impl<'a> Iterator for UsePathIter<'a> {
-    type Item = PathPart<'a, &'a Ident>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(tree) = self.tree.take() {
-            return match tree {
-                UseTree::Path(UsePath {
-                    ident, tree: next, ..
-                }) => {
-                    self.tree.replace(next);
-                    Some(ident.into())
-                }
-                UseTree::Name(UseName { ident }) => Some(ident.into()),
-                UseTree::Rename(UseRename { ident, .. }) => Some(ident.into()),
-                tree @ (UseTree::Glob(_) | UseTree::Group(_)) => Some(tree.into()),
-            };
-        }
-
-        None
-    }
-}
-
-pub struct UsePathIterMut<'a> {
-    tree: Option<&'a mut UseTree>,
-}
-
-impl<'a> Iterator for UsePathIterMut<'a> {
-    type Item = PathPart<'a, &'a mut Ident>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(tree) = self.tree.take() {
-            return match tree {
-                UseTree::Path(UsePath {
-                    ident, tree: next, ..
-                }) => {
-                    self.tree.replace(next);
-                    Some(ident.into())
-                }
-                UseTree::Name(UseName { ident }) => Some(ident.into()),
-                UseTree::Rename(UseRename { ident, .. }) => Some(ident.into()),
-                tree @ (UseTree::Glob(_) | UseTree::Group(_)) => Some(tree.into()),
-            };
-        }
-
-        None
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum PathType {
-    Path,
-    UseItem,
-}
-
-pub(crate) trait RustPath: Sized {
-    type Iter<'a>: Iterator<Item = PathPart<'a, &'a Ident>>
-    where
-        Self: 'a;
-    type IterMut<'a>: Iterator<Item = PathPart<'a, &'a mut Ident>>
+pub trait CratePath: FromIterator<Self::Part> {
+    type Part: PathPart;
+    type Iter<'a>: Iterator<Item = &'a Self::Part> + Clone
     where
         Self: 'a;
 
     fn leading_colon(&self) -> Option<PathSep>;
-    fn leading_colon_mut(&mut self) -> &mut Option<PathSep>;
-    fn iter_path(&self) -> Self::Iter<'_>;
-    fn iter_path_mut(&mut self) -> Self::IterMut<'_>;
-    fn from_path_parts<'b, I: Iterator<Item = PathPart<'b, &'b Ident>>>(
-        &self,
-        leading_colon: Option<PathSep>,
-        last_match: Option<UseTree>,
-        parts: I,
-    ) -> Self;
-    fn replace_prefix(&self, prefix_count: usize, replace_with: &syn::Path) -> Self {
-        let mut this_parts = self.iter_path().skip(prefix_count - 1);
-        let last_match = this_parts
-            .next()
-            .expect("At least one match must have occurred.");
-        self.from_path_parts(
-            replace_with.leading_colon(),
-            replace_with
-                .iter_path()
-                .chain(self.iter_path().skip(prefix_count)),
-        )
+
+    fn iter(&self) -> Self::Iter<'_>;
+}
+
+impl CratePath for syn::Path {
+    type Part = PathSegment;
+
+    type Iter<'a> = PathPartIter;
+
+    fn leading_colon(&self) -> Option<PathSep> {
+        todo!()
+    }
+
+    fn iter(&self) -> Self::Iter<'_> {
+        todo!()
     }
 }
 
-impl RustPath for syn::Path {
-    type Iter<'a> = PathIter<'a>;
-
-    type IterMut<'a> = PathIterMut<'a>;
-
-    fn leading_colon(&self) -> Option<PathSep> {
-        self.leading_colon
-    }
-
-    fn leading_colon_mut(&mut self) -> &mut Option<PathSep> {
-        &mut self.leading_colon
-    }
-
-    fn iter_path(&self) -> Self::Iter<'_> {
-        PathIter {
-            segments: self.segments.iter(),
-        }
-    }
-
-    fn iter_path_mut(&mut self) -> Self::IterMut<'_> {
-        PathIterMut {
-            segments: self.segments.iter_mut(),
-        }
-    }
-
-    fn from_path_parts<'b, I: Iterator<Item = PathPart<'b, &'b Ident>>>(
-        &self,
-        leading_colon: Option<PathSep>,
-        parts: I,
-    ) -> Self {
-        syn::Path {
-            leading_colon,
-            segments: parts
-                .map(|p| match p {
-                    PathPart::Ident(id) => PathSegment {
-                        arguments: PathArguments::None,
-                        ident: id.clone(),
-                    },
-                    PathPart::Other(o) => panic!("PathPart is {o:?} when an Ident was expected."),
-                })
-                .collect::<Punctuated<_, _>>(),
-        }
-    }
+pub enum MatchStatus<'a, P: CratePath + 'a> {
+    Begin,
+    Partial { current: &'a P::Part },
+    Complete { tail: P::Iter<'a> },
+    Different,
 }
 
-impl RustPath for ItemUse {
-    type Iter<'a> = UsePathIter<'a>;
+pub struct Matcher<'a, P: CratePath> {
+    query: &'a Query,
+    replacement: &'a Replacement,
+    target: &'a Target<P>,
+}
 
-    type IterMut<'a> = UsePathIterMut<'a>;
-
-    fn leading_colon(&self) -> Option<PathSep> {
-        self.leading_colon
-    }
-
-    fn leading_colon_mut(&mut self) -> &mut Option<PathSep> {
-        &mut self.leading_colon
-    }
-
-    fn iter_path(&self) -> Self::Iter<'_> {
-        UsePathIter {
-            tree: Some(&self.tree),
-        }
-    }
-
-    fn iter_path_mut(&mut self) -> Self::IterMut<'_> {
-        UsePathIterMut {
-            tree: Some(&mut self.tree),
-        }
-    }
-
-    fn from_path_parts<'b, I: Iterator<Item = PathPart<'b, &'b Ident>>>(
-        &self,
-        leading_colon: Option<PathSep>,
-        parts: I,
-    ) -> Self {
-        let Self {
-            attrs,
-            vis,
-            use_token,
-            semi_token,
-            ..
-        } = self;
-
+impl<'a, P: CratePath> Matcher<'a, P> {
+    fn new(
+        query: &'a Query,
+        replacement: &'a Replacement,
+        target: &'a Target<P>,
+    ) -> Matcher<'a, P> {
         Self {
-            attrs,
-            vis,
-            use_token,
-            leading_colon,
-            tree: parts.map(|p| match p {
-                PathPart::Ident(_) => todo!(),
-                PathPart::Other(_) => todo!(),
-            }),
-            semi_token,
+            query,
+            replacement,
+            target,
+        }
+    }
+
+    fn match_iter(&self) -> MatchIter<'a, P> {
+        if self.query.leading_colon == self.target.leading_colon {
+            MatchIter::LeadingColonsMatch(BaseMatchIter {
+                query_iter: self.query.segments.iter().peekable(),
+                target_iter: self.target.segments.iter(),
+                status: MatchStatus::Begin,
+            })
+        } else {
+            MatchIter::LeadingColonsMismatch
+        }
+    }
+
+    fn maybe_replace(&self) -> Option<P> {
+        self.match_iter().last().and_then(|status| match status {
+            MatchStatus::Begin => unreachable!(
+                "If MatchIter has been consumed, it should not return MatchStatus::Begin."
+            ),
+            MatchStatus::Partial { current } => unreachable!(
+                "If MatchIter has been consumed, it should not return MatchStatus::Partial."
+            ),
+            MatchStatus::Complete { tail } => Some(
+                self.replacement
+                    .path_parts_iter::<P::Part>()
+                    .chain(tail.map(<P::Part>::clone))
+                    .collect(),
+            ),
+            MatchStatus::Different => None,
+        })
+    }
+}
+
+pub enum MatchIter<'a, P: CratePath + 'a> {
+    /// Leading colons matched.
+    LeadingColonsMatch(BaseMatchIter<'a, P>),
+    /// Leading colons did not match.
+    LeadingColonsMismatch,
+}
+
+impl<'a, P: CratePath + 'a> MatchIter<'a, P> {
+    fn status(&self) -> Option<MatchStatus<'a, P>> {
+        match self {
+            MatchIter::LeadingColonsMatch(match_iter) => Some(match_iter.status),
+            MatchIter::LeadingColonsMismatch => None,
+        }
+    }
+
+    fn concat(&self, replacement: &'a Replacement) {
+        match self.status() {
+            Some(m) => todo!(),
+            None => todo!(),
         }
     }
 }
 
-impl BpafPathReplacer {
-    pub(crate) fn new(target: syn::Path, replacement: syn::Path) -> Self {
-        BpafPathReplacer {
-            target,
-            replacement,
-        }
-    }
+pub struct BaseMatchIter<'a, P: CratePath + 'a> {
+    query_iter: Peekable<slice::Iter<'a, Ident>>,
+    target_iter: P::Iter<'a>,
+    status: MatchStatus<'a, P>,
+}
 
-    /// Check if both [`target`](Self::target) and `other` have the same kind of
-    /// leading path segment (`::`), which marks [a path as
-    /// global](https://doc.rust-lang.org/reference/procedural-macros.html#procedural-macro-hygiene).
-    ///
-    /// If these do not match, no replacement will be performed.
-    fn matches_target_leading_colon<P: RustPath>(&self, other: &P) -> bool {
-        self.target.leading_colon().is_some() && other.leading_colon().is_some()
-    }
+impl<'a, P: CratePath> BaseMatchIter<'a, P> {
+    fn match_parts(
+        &mut self,
+        query_part: &'a Ident,
+        target_part: &'a P::Part,
+    ) -> MatchStatus<'a, P> {
+        self.status = match (self.query_iter.next(), self.target_iter.next()) {
+            (Some(id), Some(part)) => {
+                if id == part.ident() {
+                    if let Some(_) = self.query_iter.peek() {
+                        MatchStatus::Partial { current: part }
+                    } else {
+                        MatchStatus::Complete {
+                            tail: self.target_iter.clone(),
+                        }
+                    }
+                } else {
+                    MatchStatus::Different
+                }
+            }
+            (None, _) => match self.status {
+                MatchStatus::Begin => MatchStatus::Different,
+                MatchStatus::Partial { current } => MatchStatus::Different,
+                status @ (MatchStatus::Complete { .. } | MatchStatus::Different) => status,
+            },
+            (Some(_), None) => MatchStatus::Different,
+        };
 
-    /// Check if the initial segments of `other` match [`target`](Self::target).
-    ///
-    /// If these do not match, no replacement will be performed.
-    fn matches_target_segments<P: RustPath>(&self, other: &P) -> bool {
-        self.target
-            .iter_path()
-            .zip(other.iter_path())
-            .all(|(f, o)| f == o)
+        self.status
     }
+}
 
-    /// Replaces the prefix of `other` with those of [`replacement`](Self::replacement).
-    fn replace_prefix_if_match<P: RustPath>(&self, other: &mut P) {
-        if self.matches_target_leading_colon(other) && self.matches_target_segments(other) {
-            *other = other.replace_prefix(&self.replacement);
-        }
+impl<'a, P: CratePath> Iterator for MatchIter<'a, P> {
+    type Item = MatchStatus<'a, P>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
     }
 }
 
 impl VisitMut for BpafPathReplacer {
     fn visit_path_mut(&mut self, path: &mut syn::Path) {
-        self.replace_prefix_if_match(path);
+        if let Some(replaced) = self.replace_if_match(path) {
+            path = replaced;
+        }
         visit_mut::visit_path_mut(self, path);
     }
 
     fn visit_item_use_mut(&mut self, item_use: &mut ItemUse) {
+        if let Some(replaced) = self.replace_if_match(item_use) {
+            item_use = replaced;
+        }
         visit_mut::visit_item_use_mut(self, item_use);
     }
 }
